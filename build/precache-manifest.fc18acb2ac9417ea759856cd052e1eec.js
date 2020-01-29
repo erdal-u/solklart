3c626692c337266aaf92a24cacc531502bfb03f3
erdal.u <erdal.ustalar@gmail.com>
2020-01-29 14:34:17 +0100

    sdsd

---
+++ build/precache-manifest.fc18acb2ac9417ea759856cd052e1eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e81dd5fb2a08bdf2bd888676b60847",
+    "url": "/solklart/index.html"
+  },
+  {
+    "revision": "647d0430b27316b9f808",
+    "url": "/solklart/static/css/main.b1ece8eb.chunk.css"
+  },
+  {
+    "revision": "454f1beaf6015b4626ae",
+    "url": "/solklart/static/js/2.9079409e.chunk.js"
+  },
+  {
+    "revision": "bb92786afbef3648415ff2f31507b89a",
+    "url": "/solklart/static/js/2.9079409e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "647d0430b27316b9f808",
+    "url": "/solklart/static/js/main.27ca8525.chunk.js"
+  },
+  {
+    "revision": "a0e7128757baf5afa33a",
+    "url": "/solklart/static/js/runtime-main.2b7250a0.js"
+  },
+  {
+    "revision": "ba61dd628df0b59e40fc69465429ad4a",
+    "url": "/solklart/static/media/0.ba61dd62.jpg"
+  }
+]);
