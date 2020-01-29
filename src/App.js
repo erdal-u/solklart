ca0a2ef364ddce67e707b9eafed8386efde7951a
erdal.u <erdal.ustalar@gmail.com>
2020-01-29 14:14:02 +0100

    sdsd

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Route, Switch } from "react-router-dom";
-import { Economy } from "./Components/Economy";
-import { IT } from "./Components/IT/IT";
-import { Salary } from "./Components/Salary/Salary";
-import { NoMatch } from "./Components/NoMatch";
-
-import { Layout } from "./Components/Layout";
-import { NavigationBar } from "./Components/NavigationBar";
-import { Mycases } from "./Components/Mycases";
-import { Case } from "./Components/Salary/Case";
-import { Blanketter } from "./Components/Blanketter";
-import { Absence } from "./Components/Salary/Absence";
-import { Missonfee } from "./Components/Salary/Missonfee";
-import { Schema } from "./Components/Salary/Schema";
-import { Certificate } from "./Components/Salary/Certificate";
-import { Selfservice } from "./Components/Salary/Selfservice";
-import { SupportIT } from "./Components/IT/SupportIT";
-import Order from "./Components/IT/Order";
-import { Solklart } from "./Components/Solklart";
-import history from "./Components/history";
+import Header from "./Components/Header/Header";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <React.Fragment>
-        <HashRouter history={history}>
-          <NavigationBar />
-          <Layout>
-            <Switch>
-              <Route path="/solklart" component={Solklart} />
-              <Route path="/economy" component={Economy} />
-              <Route path="/it" component={IT} />
-              <Route path="/salary" component={Salary} />
-              <Route path="/mycases" component={Mycases} />
-              <Route path="/blanketter" component={Blanketter} />
-              <Route path="/case" component={Case} />
-              <Route path="/absence" component={Absence} />
-              <Route path="/missonfee" component={Missonfee} />
-              <Route path="/selfservice" component={Selfservice} />
-              <Route path="/schema" component={Schema} />
-              <Route path="/certificate" component={Certificate} />
-              <Route path="/supportit" component={SupportIT} />
-              <Route path="/order" component={Order} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Layout>
-        </Router>
-      </React.Fragment>
-    </div>
+    <HashRouter>
+      <Header />
+    </HashRouter>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,51 @@
-import React, { Component } from "react";
-import Body from "../Body/Body";
-import "./Header.css";
-import { Jumbotron } from "react-bootstrap";
+import React from "react";
+import "../../App.css";
+import { Route, Switch } from "react-router-dom";
+import { Economy } from "./Components/Economy";
+import { IT } from "./Components/IT/IT";
+import { Salary } from "./Components/Salary/Salary";
+import { NoMatch } from "./Components/NoMatch";
 
-export class Header extends Component {
-  render() {
-    return (
-      <Jumbotron fluid className="bgImg">
-        <Body />
-        <p>detta är ett test</p>
-      </Jumbotron>
-    );
-  }
+import { Layout } from "./Components/Layout";
+import { NavigationBar } from "./Components/NavigationBar";
+import { Mycases } from "./Components/Mycases";
+import { Case } from "./Components/Salary/Case";
+import { Blanketter } from "./Components/Blanketter";
+import { Absence } from "./Components/Salary/Absence";
+import { Missonfee } from "./Components/Salary/Missonfee";
+import { Schema } from "./Components/Salary/Schema";
+import { Certificate } from "./Components/Salary/Certificate";
+import { Selfservice } from "./Components/Salary/Selfservice";
+import { SupportIT } from "./Components/IT/SupportIT";
+import Order from "./Components/IT/Order";
+import { Solklart } from "./Components/Solklart";
+import history from "./Components/history";
+
+function Header() {
+  return (
+    <div>
+      <NavigationBar />
+      <Layout>
+        <Switch>
+          <Route path="/" component={Solklart} />
+          <Route path="/economy" component={Economy} />
+          <Route path="/it" component={IT} />
+          <Route path="/salary" component={Salary} />
+          <Route path="/mycases" component={Mycases} />
+          <Route path="/blanketter" component={Blanketter} />
+          <Route path="/case" component={Case} />
+          <Route path="/absence" component={Absence} />
+          <Route path="/missonfee" component={Missonfee} />
+          <Route path="/selfservice" component={Selfservice} />
+          <Route path="/schema" component={Schema} />
+          <Route path="/certificate" component={Certificate} />
+          <Route path="/supportit" component={SupportIT} />
+          <Route path="/order" component={Order} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
-export default Header;
+export default App;
