3340b57a5c486072636499c83d5f7220fe87fcfb
erdal.u <erdal.ustalar@gmail.com>
2020-01-30 15:38:00 +0100

    senaste

---
+++ build/precache-manifest.e975202b5a3938330961ec461ce4304c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4878807e944e2a3ab0c16ebf78821e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f002516a6ab7dc16b8d0",
+    "url": "/static/css/main.270c40b4.chunk.css"
+  },
+  {
+    "revision": "e8100cf9cd49f86df2a6",
+    "url": "/static/js/2.96ac584d.chunk.js"
+  },
+  {
+    "revision": "bb92786afbef3648415ff2f31507b89a",
+    "url": "/static/js/2.96ac584d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f002516a6ab7dc16b8d0",
+    "url": "/static/js/main.5d9c38bf.chunk.js"
+  },
+  {
+    "revision": "085f46ef294b2ba2e292",
+    "url": "/static/js/runtime-main.02808ec3.js"
+  },
+  {
+    "revision": "ba61dd628df0b59e40fc69465429ad4a",
+    "url": "/static/media/0.ba61dd62.jpg"
+  }
+]);
